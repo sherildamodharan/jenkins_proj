03e088a0c7ea1f3027b945b87828a2d23d077a50
Ubuntu <jenkins@jen.1td4epdow1fe3k4xmne2fgscqa.bx.internal.cloudapp.net>
2020-04-22 09:24:25 +0000

    new

--- new.py
+++ new.py
@@ -1,24 +1,4 @@
-def jpeg_res(filename):
-   """"This function prints the resolution of the jpeg image file passed into it"""
-
-   # open image for reading in binary mode
-   with open(filename,'rb') as img_file:
-
-       # height of image (in 2 bytes) is at 164th position
-       img_file.seek(163)
-
-       # read the 2 bytes
-       a = img_file.read(2)
-
-       # calculate height
-       height = (a[0] << 8) + a[1]
-
-       # next 2 bytes is width
-       a = img_file.read(2)
-
-       # calculate width
-       width = (a[0] << 8) + a[1]
-
-   print("The resolution of the image is",width,"x",height)
-
-jpeg_res("img1.jpg")
+# Program to display calendar of the given month and year importing calendar module
+import calendar yy = 2014 # year mm = 11 # month
+# To take month and year input from the user yy = int(input("Enter year: ")) mm = int(input("Enter month: ")) display the calendar
+print(calendar.month(yy, mm))
